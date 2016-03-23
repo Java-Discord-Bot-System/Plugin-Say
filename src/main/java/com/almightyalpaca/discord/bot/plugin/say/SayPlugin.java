09235d06eb880dafdcc0f8cdf1b0b1490014b33d
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-03-23 18:51:49 +0100

    Initial commit

---
+++ src/main/java/com/almightyalpaca/discord/bot/plugin/say/SayPlugin.java
@@ -0,0 +1,48 @@
+package com.almightyalpaca.discord.bot.plugin.say;
+
+import com.almightyalpaca.discord.bot.system.command.AbstractCommand;
+import com.almightyalpaca.discord.bot.system.command.annotation.Command;
+import com.almightyalpaca.discord.bot.system.command.arguments.special.Rest;
+import com.almightyalpaca.discord.bot.system.events.CommandEvent;
+import com.almightyalpaca.discord.bot.system.exception.PluginLoadingException;
+import com.almightyalpaca.discord.bot.system.exception.PluginUnloadingException;
+import com.almightyalpaca.discord.bot.system.plugins.Plugin;
+import com.almightyalpaca.discord.bot.system.plugins.PluginInfo;
+
+import net.dv8tion.jda.Permission;
+import net.dv8tion.jda.utils.PermissionUtil;
+
+public class SayPlugin extends Plugin {
+
+	class SayCommand extends AbstractCommand {
+
+		public SayCommand() {
+			super("say", "Let me say something", "");
+		}
+
+		@Command(dm = true, guild = true, async = true)
+		private void onCommand(final CommandEvent event, final Rest rest) {
+			String message = "\u180E" + rest.getString();
+			if (event.isGuild()) {
+				if (!PermissionUtil.checkPermission(event.getAuthor(), Permission.MESSAGE_MENTION_EVERYONE, event.getTextChannel())) {
+					message = message.replaceAll("@everyone", "@\u180Eeveryone");
+				}
+			}
+			event.sendMessage(message);
+		}
+	}
+
+	private static final PluginInfo INFO = new PluginInfo("com.almightyalpaca.discord.bot.plugin.say", "1.0.0", "Almighty Alpaca", "Say Plugin", "Just to Say");
+
+	public SayPlugin() {
+		super(SayPlugin.INFO);
+	}
+
+	@Override
+	public void load() throws PluginLoadingException {
+		this.registerCommand(new SayCommand());
+	}
+
+	@Override
+	public void unload() throws PluginUnloadingException {}
+}
